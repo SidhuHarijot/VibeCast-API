ed8144a06e82acdf365203a1ca0319691a0453f0
Harijot Singh <125294510+SidhuHarijot@users.noreply.github.com>
2024-04-23 17:54:39 -0600

    updated requirements

--- testing.py
+++ testing.py
@@ -1,35 +1,12 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import pylast
-from decouple import config
+import requests
 
+baseurl = "https://vibecast-api.onrender.com/"
 
-spotify_id = config("SPOTIFY_CLIENT_ID")
-spotify_secret = config("SPOTIFY_CLIENT_SECRET")
+# try to get the tracks from the playlist
+response = requests.get(baseurl + "top-50/US")
 
-lastfm_api_key = config("LASTFM_API")
-lastfm_api_secret = config("LASTFM_SHARED_SECRET")
-lastfm_username = config("LASTFM_USERNAME")
-lastfm_password = pylast.md5(config("LASTFM_PASSWORD"))
-
-client_credentials_manager = SpotifyClientCredentials(client_id=spotify_id, client_secret=spotify_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-result = sp.search(q="top50", type="playlist", limit=1, market="US")
-playlistId = result["playlists"]["items"][0]["id"]
-print(playlistId)
-
-result = sp.playlist(playlistId, fields="tracks")
-
-print(result["tracks"]["items"][0]["track"]["name"])
-
-username = "your_user_name"
-password_hash = pylast.md5("your_password")
-
-network = pylast.LastFMNetwork(
-    api_key=lastfm_api_key,
-    api_secret=lastfm_api_secret,
-    username=lastfm_username,
-    password_hash=lastfm_password,
-)
-
+# check if the request was successful
+if response.status_code == 200:
+    print(response.json())
+elif response.status_code == 401:
+    requests.get(baseurl + "login")
